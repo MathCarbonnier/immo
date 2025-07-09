79c2ef7d00453fbe2e2c45900648ced2e4053162
mce <mathieu.carbonnier@ville-ge.ch>
2025-07-09 17:06:24 +0200

    Ajustement de la taille de la page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container mt-4">
-      <router-outlet></router-outlet>
-    </div>
+      <div class="app-container">
+        <router-outlet></router-outlet>
+      </div>
   `,
-  styles: []
+  styles: [`
+    .app-container {
+      padding: 20px;
+      max-width: 80%;
+      margin: 0 auto;
+    }
+  `]
 })
 export class AppComponent {
   title = 'immo-frontend';
